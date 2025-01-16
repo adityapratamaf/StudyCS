7facee7507fb929e69865546bc7c453121c54312
unknown <Administrator@PM02SHUDD001.pertamina.com>
2025-01-16 14:29:24 +0700

    4

--- Application/Controllers/GenreController.cs
+++ Application/Controllers/GenreController.cs
@@ -42,6 +42,50 @@ namespace Application.Controllers
             return RedirectToAction("Index");
         }
 
+        public IActionResult Edit(int id)
+        {
+            var item = _context.Genres.FirstOrDefault(b => b.Id == id);
+
+            if (item != null)
+            {
+                ViewBag.Genres = _context.Genres.ToList();
+                return View("Edit", item);
+            }
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Update(Genre model)
+        {
+            if (!ModelState.IsValid)
+            {
+                // jika model tidak valid, redirect ke Index dengan pesan error
+                TempData["ErrorMessage"] = "invalid data";
+                return RedirectToAction("Index");
+            }
+
+            // cari data berdasarkan ID
+            var item = _context.Genres.FirstOrDefault(g => g.Id == model.Id);
+
+            if (item != null)
+            {
+                // memperbarui properti genre yang ditemukan
+                item.Name_Genre = model.Name_Genre;
+
+                // simpan perubahan ke database
+                _context.SaveChanges();
+
+                // menampilkan pesan sukses
+                TempData["SuccessMessage"] = "data successfully updated!";
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                // jika item tidak ditemukan
+                TempData["ErrorMessage"] = "data not found.";
+                return RedirectToAction("Index");
+            }
+        }
+
         public IActionResult Delete(int id)
         {
             // periksa apakah _context.Genres tidak null
--- Application/obj/Debug/net6.0/Application.AssemblyInfo.cs
+++ Application/obj/Debug/net6.0/Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1edb128007d070d4656b699c43bf995b780dd539")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+89a95a729a403ab293d9555ef487cf8672170c81")]
 [assembly: System.Reflection.AssemblyProductAttribute("Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
